e2d5b1e8e12d8b0f279f853dee996aca64eca2ec
jspangled <jes3277@rit.edu>
2021-11-21 23:32:47 -0500

    Prog on q1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Assumes little-endian for binary
--- q1.c
+++ q1.c
@@ -1,28 +1,52 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdint.h>
 
+// First 20 bits:
+// Test 00000000000000000100111000010010 
+// Mask 11111111111111111111000000000000
+// Page# 0100 = 4
+
+// Last 12 bits:
 // Test & (1 << 12) - 1 = Test & (Mask2 - 1) = Test & Mask = Offset
 // Test   100111000010010
-// Mask2    1000000000000
-// 1                    1
 // Mask      111111111111 
 // Offset    111000010010
-
-void main() {
-    int test = 19986;
+void physicalAddrExtraction(int addr) {
     int m = 20;
     int n = 12;
-    unsigned mask, mask2; 
+    unsigned pageNumMask = 0xFFFFF000;
+    unsigned offsetMask = (1 << n) - 1;
     int pageNum, startAddr, offset;
 
     // First m digits
-   
+    pageNum = addr & pageNumMask;
+    pageNum = pageNum >> n; 
+
+    // Physical address starts at most significant bit
+    int msb = 0;
+    int addr2 = addr;
+    addr2 = addr2 / 2;
+    while (addr2 != 0) {
+        addr2 = addr2 / 2;
+        msb++;
+    }
+    msb = 1 << msb;
+ 
     // Last n digits
-    mask = (1 << n) - 1;    
-    mask2 = 1 << n;
-    offset = test & mask;
+    offset = addr & offsetMask;
     
     // Print results
-    printf("Mask = %d\n", mask);
-    printf("Mask2 = %d\n", mask2);
+    printf("Physical address = %d\n", addr);
+    printf("Page number = %d\n", pageNum); 
+    printf("Starting address = 0x%04x\n", msb);
     printf("Offset = %d\n", offset);   
+
+}
+
+void main() {
+    int addrs[7] = { 1, 256, 32768, 32769, 128, 65534, 33153 };
+    for (int i = 0; i < 7; i++) {
+        physicalAddrExtraction(addrs[i]);
+        printf("\n");
+    } 
 }
