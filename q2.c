1bce9c8586a55b337f7b4ee3ab13c50092f3f206
jspangled <jes3277@rit.edu>
2021-11-29 20:18:53 -0500

    Allocate page table

--- q2.c
+++ q2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define BUF_SIZE 256
+#include <string.h>
+#define SIZE 256
+#define pageTableIndex(x) (x * SIZE)
+
 
 // Assumes little endian
 void printBits(size_t const size, void const * const ptr)
@@ -31,27 +34,33 @@ void logicalAddrExtraction(int addr, int* pageNum, int* offset) {
 }
 
 int main() {
-	// Create page table	
-	char* page_table = malloc(BUF_SIZE * BUF_SIZE);
-
+	// Create page table, fill each page with a character of this string	
+    char* str = "UeRDK4SrzAcDQV6PgGjsLEf6oTJfENypgAMxdIegMDWbOMt3wfvNjlAoxFV2CbUHoRZZN2cBcUwOAN676KgB3Xb4zJul89VKOQvl8ZjCnxRj8109OoOtZihftshnnKrjmOt8ZkFm8BO9gNGPXLDvlwyGRm4CSPdKaSSitxk30uDEyUZrEc1wfRJLIST7q6SN1F1jbz4IBJI9C4APb4KU6yDPbqovXfgka7ce7C5V6cQdS1z9JSKibN8PTcwgdzuS";
+	char* pageTable = malloc(SIZE * SIZE * sizeof(char));
+    for (int i = 0; i < SIZE; i++) {
+        printf("%p\n", (pageTable + pageTableIndex(i)));
+        memset(pageTable + pageTableIndex(i), str[i], sizeof(char));    
+    }
+     
 	// unsigned long tlb[BUF_SIZE];	
 	// for (int i = 0; i < BUF_SIZE; i++) {
     // 		tlb[i] = i*256;
 	//}
-	
 	//unsigned long thing = (unsigned long) page_table + tlb[0]; 
 	
 	// Read in addresses.txt
 	FILE* addresses = fopen("addresses.txt", "r");
-	char line[BUF_SIZE];
-	while (fgets(line, BUF_SIZE, addresses)) {
+	char line[SIZE];
+	while (fgets(line, SIZE, addresses)) {
 		int logicalAddr, pageNum, offset;
 		logicalAddr = atoi(line);
 		logicalAddrExtraction(logicalAddr, &pageNum, &offset);
-		printf("address: %d\npage number: %d\noffset: %d\n\n", logicalAddr, pageNum, offset);
+		//printf("address: %d\npage number: %d\noffset: %d\n\n", logicalAddr, pageNum, offset);
+        // Extract frame number from page table
+            
 	}	
-
-	free(page_table);
+    
+	free(pageTable);
 	fclose(addresses);	
 	return 0;
 }
